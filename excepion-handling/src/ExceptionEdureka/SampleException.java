367f79d82edd53a73b27c6465185d975c1c3b5b5
Isak Silva <isakfreires98@gmail.com>
2024-10-23 14:36:26 -0300

    Change Gitignore

--- excepion-handling/src/ExceptionEdureka/SampleException.java
+++ excepion-handling/src/ExceptionEdureka/SampleException.java
@@ -1,4 +1,16 @@
 package ExceptionEdureka;
 
 public class SampleException {
+    public static void main(String[] args) {
+        String str = null;
+//        System.out.println(str.length());
+        try {
+            int a = 30, b = 0;
+            int c = a/b;
+            System.out.println(c);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Can't divide a number by zero");
+        }
+    }
 }
--- lambda-functions/src/expressions/functionInVariable.java
+++ lambda-functions/src/expressions/functionInVariable.java
@@ -1,4 +1,19 @@
 package expressions;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class functionInVariable {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> addOne = x -> x + 1;
+// lambda assigned to a reference, where
+// Function has an input and an output:
+        int result1 = addOne.apply(123);
+        System.out.println(result1);
+
+// a method assigned to a reference, where
+// Consumer takes an input (void return):
+
+    }
 }
